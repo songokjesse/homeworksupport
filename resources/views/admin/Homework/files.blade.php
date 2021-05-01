e13e2b1bd68d590004e98c857f12a8fa57de8623
jessesongok <jessesongok@gmail.com>
2021-05-01 12:14:23 +0300

    Removed livewire homework file display component

--- resources/views/admin/Homework/files.blade.php
+++ resources/views/admin/Homework/files.blade.php
@@ -49,7 +49,40 @@
                                         <br/>
 
                                     {{-- Display Uploaded Files--}}
-                                        <livewire:file :homework="$homework"/>
+                                        <div class="flex flex-col">
+                                            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                                        <table class="table-auto min-w-full divide-y divide-gray-200">
+                                                            <thead class="bg-gray-50">
+                                                            <tr>
+                                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">#</th>
+                                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Files</th>
+                                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                                                            </tr>
+                                                            </thead>
+                                                            <tbody class="bg-white divide-y divide-gray-200">
+                                                            @foreach($homework as $work)
+                                                                <tr>
+                                                                    <td class="px-6 py-4 whitespace-nowrap">{{$loop->iteration}}</td>
+                                                                    <td class="px-6 py-4 whitespace-nowrap">
+                                                                        {{$work->filePath}}
+                                                                    </td>
+                                                                    <td class="px-6 py-4 whitespace-nowrap">
+                                                                        <a href="#"><button class="text-indigo-600 hover:text-indigo-900">Edit</button></a>
+                                                                        <a href="#" ><button class="text-red-600 hover:text-red-900">Delete</button></a>
+                                                                    </td>
+                                                                </tr>
+                                                            @endforeach
+                                                            </tbody>
+                                                        </table>
+
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
                                     </div>
                                     </div>
                                 </div>
