77e6cf6725a2a25b1b76230b1f7d9adee79bbcee
jessesongok <jessesongok@gmail.com>
2021-05-04 11:03:01 +0300

    Add session status to index views

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,6 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div>
                         <div>
