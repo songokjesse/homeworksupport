622c442c5888470cd1b8b6f2ee34b8da80dc41ef
Jesse Songok <jessesongok@gmail.com>
2021-05-06 19:55:30 +0300

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-    protected $fillable = ['categoryName'];
+    protected $fillable = ['categoryName', 'url'];
 
     public function homeworks()
     {
