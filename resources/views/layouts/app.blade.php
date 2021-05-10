b1333b5b3061164594099fdb456369d2a0071c86
jessesongok <jessesongok@gmail.com>
2021-05-10 13:43:47 +0300

    disable whatsapp image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
-                <img src="https://res.cloudinary.com/homework-support-com/image/upload/v1620641856/files/whatsapp_nbkmsj.png"/>
+{{--                <img src="https://res.cloudinary.com/homework-support-com/image/upload/v1620641856/files/whatsapp_nbkmsj.png"/>--}}
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
