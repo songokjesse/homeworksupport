fd759d17aba95f71cd69f0cdf6a1ccd38ffc89de
jessesongok <jessesongok@gmail.com>
2021-05-18 21:15:04 +0300

    Added Price colomn to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
                         <tr>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Homework</th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
                         </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
@@ -65,9 +65,12 @@
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <a href="{{route('show', $work->id)}}">
                                     {!! html_entity_decode( Str::limit($work->name, 100)) !!}
+                                    </a>
                                 </td>
-                                </a>
-                                <td></td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <span class="text-gray-500 sm:text-sm">$ </span>
+                                    {{$work->price}}
+                                </td>
                             </tr>
 
                         @endforeach
