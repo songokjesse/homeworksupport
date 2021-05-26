31371cd073a958c6d92152354a88ca51ca20a0cf
jessesongok <jessesongok@gmail.com>
2021-05-26 21:19:06 +0300

    added email verification to user model added the tinyMC to contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -80,6 +80,16 @@
         </div>
     </main>
 </div>
+<script>
+    tinymce.init({
+        selector: 'textarea',
+        plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+        toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+        toolbar_mode: 'floating',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+    });
+</script>
 </body>
 </html>
 
