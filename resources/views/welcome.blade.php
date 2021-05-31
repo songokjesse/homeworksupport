acde6005ab498731e760091fea9de096c4c51a66
jessesongok <jessesongok@gmail.com>
2021-05-31 10:15:22 +0300

    Removed description

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
                         <tr>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Homework</th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>
+{{--                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>--}}
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
                         </tr>
                         </thead>
@@ -68,11 +68,11 @@
                                     {!! html_entity_decode( Str::limit($work->name,50)) !!}
                                     </a>
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <a href="{{route('show', $work->id)}}">
-                                    {!! html_entity_decode( Str::limit($work->description,100)) !!}
-                                    </a>
-                                </td>
+{{--                                <td class="px-6 py-4 whitespace-nowrap">--}}
+{{--                                    <a href="{{route('show', $work->id)}}">--}}
+{{--                                    {!! html_entity_decode( Str::limit($work->description,100)) !!}--}}
+{{--                                    </a>--}}
+{{--                                </td>--}}
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <span class="text-gray-500 sm:text-sm">$ </span>
                                     {{$work->price}}
