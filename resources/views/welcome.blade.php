f86c3f9ef9de5439aa4261f8beb6a644cdfac6b7
jessesongok <jessesongok@gmail.com>
2021-05-31 10:03:13 +0300

    Included description

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,6 +50,7 @@
                         <tr>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Homework</th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
                         </tr>
                         </thead>
@@ -64,7 +65,12 @@
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <a href="{{route('show', $work->id)}}">
-                                    {!! html_entity_decode( Str::limit($work->name,100)) !!}
+                                    {!! html_entity_decode( Str::limit($work->name,50)) !!}
+                                    </a>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="{{route('show', $work->id)}}">
+                                    {!! html_entity_decode( Str::limit($work->description,100)) !!}
                                     </a>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
