979b14149a655b51a46497029a00fb7f434cc08a
jessesongok <jessesongok@gmail.com>
2021-06-10 10:03:44 +0300

    Renamed menu name

--- resources/views/client/myHomework.blade.php
+++ resources/views/client/myHomework.blade.php
@@ -27,7 +27,7 @@
 {{--                                </td>--}}
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <a href="{{route('my_answers', $work->id)}}">
-                                    {!! html_entity_decode( Str::limit($work->description,100)) !!}
+                                    {!! html_entity_decode( Str::limit($work->description,500)) !!}
                                     </a>
                                 </td>
                             </tr>
