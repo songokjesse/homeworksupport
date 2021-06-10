f3af65d9c5a57b91afdc54b92750085c86c6082b
jessesongok <jessesongok@gmail.com>
2021-06-10 10:05:08 +0300

    Renamed menu name

--- resources/views/client/myHomework.blade.php
+++ resources/views/client/myHomework.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <table class="table-fixed min-w-full divide-y divide-gray-200">
+                    <table class="table-auto min-w-full divide-y divide-gray-200">
                         <thead class="bg-gray-50">
                         <tr>
 {{--                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Homework</th>--}}
