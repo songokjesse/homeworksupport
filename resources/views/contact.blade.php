80f9c5115251e89f81b3cb790ff070d82f780c4e
codelab <jessesongok@gmail.com>
2021-06-15 13:12:34 +0300

    Removed tinymc premium plugins

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -85,8 +85,8 @@
 <script>
     tinymce.init({
         selector: 'textarea',
-        plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-        toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+        plugins: ' linkchecker autolink lists media pageembed table ',
+        toolbar: 'addcomment showcomments  checklist code   table',
         toolbar_mode: 'floating',
         tinycomments_mode: 'embedded',
         tinycomments_author: 'Author name',
