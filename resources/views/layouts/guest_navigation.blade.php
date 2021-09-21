667c2192f2b0b9cdc12b38f9a52dcb357efdc1ee
chamgei <83578322+chamgei@users.noreply.github.com>
2021-09-21 13:40:22 +0300

    Update guest_navigation.blade.php

--- resources/views/layouts/guest_navigation.blade.php
+++ resources/views/layouts/guest_navigation.blade.php
@@ -19,7 +19,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('aboutUs')" :active="request()->routeIs('aboutUs')">
-                        {{ __('AboutUs') }}
+                        {{ __('About Us') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
